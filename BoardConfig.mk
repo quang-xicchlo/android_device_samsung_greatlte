e0705551e89e302ba9c2b956e239c72b58717419
Dyneteve <sound0020@gmail.com>
2018-08-17 12:34:30 +0300

    Update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,7 @@
 # Platform
 BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := exynos5
-TARGET_SOC := universal8895
+TARGET_SOC := exynos8895
 TARGET_BOOTLOADER_BOARD_NAME := universal8895
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
@@ -28,6 +28,7 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
@@ -38,7 +39,8 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 # Kernel
 TARGET_PREBUILT_KERNEL := device/samsung/greatlte/kernel
 BOARD_KERNEL_BASE := 0x10000000
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/greatlte/dt.img
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SRPQC03B001KU --dt device/samsung/greatlte/dt.img
 
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
@@ -50,8 +52,8 @@ TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
-TW_MAX_BRIGHTNESS := 255
-TW_DEFAULT_BRIGHTNESS := 162
+TW_MAX_BRIGHTNESS := 36600
+TW_DEFAULT_BRIGHTNESS := 16200
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
 TW_INCLUDE_NTFS_3G := true
